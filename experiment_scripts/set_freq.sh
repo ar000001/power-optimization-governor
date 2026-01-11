b267e212145d8673e22d79555b7ad4881f05885d
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2026-01-11 19:56:15 +0100

    Removed minor syntax error. Elif instead of else if.

--- experiment_scripts/set_freq.sh
+++ experiment_scripts/set_freq.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 CpuType=$1
@@ -12,12 +11,10 @@ if [ "$CpuType" == "little" ]; then
         echo "Error: Freq must be a valid frequency for the little CPU"
         exit 1
     fi
-else
-    if [ "$CpuType" == "big" ]; then
-        if ! [[ " ${BigFrequencyTable[@]} " =~ " ${Freq} " ]]; then
-            echo "Error: Freq must be a valid frequency for the big CPU"
-            exit 1
-        fi
+elif [ "$CpuType" == "big" ]; then
+    if ! [[ " ${BigFrequencyTable[@]} " =~ " ${Freq} " ]]; then
+        echo "Error: Freq must be a valid frequency for the big CPU"
+        exit 1
     fi
 else
     echo "Error: CpuType must be either 'little' or 'big'"
@@ -29,7 +26,17 @@ adb -d root
 if [ "$CpuType" == "little" ]; then
     adb -d shell "echo performance > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor"
     adb -d shell "echo ${Freq} > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq"
+
+    if [ "$(adb -d shell "cat /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq")" != "${Freq}" ]; then
+        echo "Error: Frequency was not set correctly"
+        exit 1
+    fi
 else
     adb -d shell "echo performance > /sys/devices/system/cpu/cpufreq/policy2/scaling_governor"
     adb -d shell "echo ${Freq} > /sys/devices/system/cpu/cpufreq/policy2/scaling_max_freq"
+
+    if [ "$(adb -d shell "cat /sys/devices/system/cpu/cpufreq/policy2/scaling_max_freq")" != "${Freq}" ]; then
+        echo "Error: Frequency was not set correctly"
+        exit 1
+    fi
 fi
