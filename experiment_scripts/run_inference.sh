865373a18630e2fe72d5d889450e7e1da235d52b
Matteo D'Urso <67522359+MatteoDrso@users.noreply.github.com>
2026-01-14 13:44:25 +0100

    Removed target flag, as it is inferred by the compiler

--- experiment_scripts/run_inference.sh
+++ experiment_scripts/run_inference.sh
@@ -7,4 +7,5 @@ PartitionPoint2=$4
 Order=$5
 
 adb -d root
-adb -d shell "cd /data/local/Working_dir && ./${Graph} --threads=4  --threads2=2  --target=NEON --n=${N_Frames} --partition_point=${PartitionPoint1} --partition_point2=${PartitionPoint2} --order=${Order}"
+adb -d shell "cd /data/local/Working_dir && ./${Graph} --threads=4  --threads2=2 --target=CL --n=${N_Frames} --partition_point=${PartitionPoint1} --partition_point2=${PartitionPoint2} --order=${Order} > last_run_output.txt"
+adb -d pull /data/local/Working_dir/last_run_output.txt last_run_output.txt
